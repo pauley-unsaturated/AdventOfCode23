da91df642b48be0b166365341324d4ab871ab794
Mark Pauley <pauley@unsaturated.net>
2023-12-03 23:07:58 -0800

    Day 2, Part 2

--- 02/Sources/main.swift
+++ 02/Sources/main.swift
@@ -14,6 +14,7 @@ let maxCubes: [Color: Int] = [.red: 12, .green: 13, .blue: 14]
 let file = CommandLine.arguments[1]
 let input = try! String(contentsOfFile: file, encoding: .utf8).split(separator: "\n")
 
+// Part 1
 let part_1 = input.map{ line in
     { match in
         (Int(match.1)!,
@@ -28,3 +29,21 @@ let part_1 = input.map{ line in
 }.reduce(0){ $0 + ($1.1 ? $1.0 : 0)}
 
 print("Part 1: \(part_1)")
+
+
+// Part 2
+let part_2 = input.map{ line in
+    { match in
+        match.1.split(separator:"; ").flatMap{
+            $0.split(separator:", ").map {
+                { (components:[Substring]) in
+                    (Int(components[0])!,Color(rawValue:String(components[1]))!)
+                }($0.split(separator:" "))
+            }
+        }.reduce(into:[Color.red:0,Color.green:0,Color.blue:0]) {
+            $0[$1.1] = max($0[$1.1]!, $1.0)
+        }.values.reduce(1,*)
+    }((try! /Game \d+: (.*)/.wholeMatch(in:line))!)
+}.reduce(0,+)
+
+print("Part 2: \(part_2)")
